2f17e0f11f7c3848db26bf29536d2778eb32cac1
Cao Duy Vu (vu@appiphany.com.au) <vu@appiphany.com.au>
2013-09-10 11:03:49 +0700

    update widget
    update widget

--- MydBatWid/src/com/kienlt/searchappwidget/SearchActivity.java
+++ MydBatWid/src/com/kienlt/searchappwidget/SearchActivity.java
@@ -30,13 +30,14 @@ public class SearchActivity extends Activity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.autocomplete_1);
         
-			try {
-				components = getInstalledComponentList();
-				mMapAppPackage = getInstalledAppList();
-			} catch (NameNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+		try {
+			components = getInstalledComponentList();
+			mMapAppPackage = getInstalledAppList();
+		} catch (NameNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_dropdown_item_1line, components);
         AutoCompleteTextView textView = (AutoCompleteTextView) findViewById(R.id.edit);
@@ -114,20 +115,20 @@ public class SearchActivity extends Activity {
     	return list;
     }
     
-    private List<String> getAllPackageName() {
-    	List<String> packageName = new ArrayList<String>();
-    	final PackageManager pm = getPackageManager();
-    	//get a list of installed apps.
-    	List<ApplicationInfo> packages = pm.getInstalledApplications(PackageManager.GET_META_DATA);
-
-    	for (ApplicationInfo packageInfo : packages) {
-    	    Log.d("KienLT", "Installed package :" + packageInfo.packageName);
-    	    Log.d("KienLT", "Launch Activity :" + pm.getLaunchIntentForPackage(packageInfo.packageName));
-    	    packageName.add(packageInfo.packageName);
-    	}
-    	
-    	return packageName;
-    }
+//    private List<String> getAllPackageName() {
+//    	List<String> packageName = new ArrayList<String>();
+//    	final PackageManager pm = getPackageManager();
+//    	//get a list of installed apps.
+//    	List<ApplicationInfo> packages = pm.getInstalledApplications(PackageManager.GET_META_DATA);
+//
+//    	for (ApplicationInfo packageInfo : packages) {
+//    	    Log.d("KienLT", "Installed package :" + packageInfo.packageName);
+//    	    Log.d("KienLT", "Launch Activity :" + pm.getLaunchIntentForPackage(packageInfo.packageName));
+//    	    packageName.add(packageInfo.packageName);
+//    	}
+//    	
+//    	return packageName;
+//    }
 //    private List<String> getInstalledComponentList() {
 //        Intent componentSearchIntent = new Intent();
 //        componentSearchIntent.addCategory(Constants.COMPONENTS_INTENT_CATEGORY);
@@ -144,47 +145,4 @@ public class SearchActivity extends Activity {
 //        return componentList;
 //    }
 
-    static final String[] COUNTRIES = new String[] {
-	"Afghanistan", "Albania", "Algeria", "American Samoa", "Andorra",
-	"Angola", "Anguilla", "Antarctica", "Antigua and Barbuda", "Argentina",
-	"Armenia", "Aruba", "Australia", "Austria", "Azerbaijan",
-	"Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium",
-	"Belize", "Benin", "Bermuda", "Bhutan", "Bolivia",
-	"Bosnia and Herzegovina", "Botswana", "Bouvet Island", "Brazil", "British Indian Ocean Territory",
-	"British Virgin Islands", "Brunei", "Bulgaria", "Burkina Faso", "Burundi",
-	"Cote d'Ivoire", "Cambodia", "Cameroon", "Canada", "Cape Verde",
-	"Cayman Islands", "Central African Republic", "Chad", "Chile", "China",
-	"Christmas Island", "Cocos (Keeling) Islands", "Colombia", "Comoros", "Congo",
-	"Cook Islands", "Costa Rica", "Croatia", "Cuba", "Cyprus", "Czech Republic",
-	"Democratic Republic of the Congo", "Denmark", "Djibouti", "Dominica", "Dominican Republic",
-	"East Timor", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea",
-	"Estonia", "Ethiopia", "Faeroe Islands", "Falkland Islands", "Fiji", "Finland",
-	"Former Yugoslav Republic of Macedonia", "France", "French Guiana", "French Polynesia",
-	"French Southern Territories", "Gabon", "Georgia", "Germany", "Ghana", "Gibraltar",
-	"Greece", "Greenland", "Grenada", "Guadeloupe", "Guam", "Guatemala", "Guinea", "Guinea-Bissau",
-	"Guyana", "Haiti", "Heard Island and McDonald Islands", "Honduras", "Hong Kong", "Hungary",
-	"Iceland", "India", "Indonesia", "Iran", "Iraq", "Ireland", "Israel", "Italy", "Jamaica",
-	"Japan", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "Kuwait", "Kyrgyzstan", "Laos",
-	"Latvia", "Lebanon", "Lesotho", "Liberia", "Libya", "Liechtenstein", "Lithuania", "Luxembourg",
-	"Macau", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands",
-	"Martinique", "Mauritania", "Mauritius", "Mayotte", "Mexico", "Micronesia", "Moldova",
-	"Monaco", "Mongolia", "Montserrat", "Morocco", "Mozambique", "Myanmar", "Namibia",
-	"Nauru", "Nepal", "Netherlands", "Netherlands Antilles", "New Caledonia", "New Zealand",
-	"Nicaragua", "Niger", "Nigeria", "Niue", "Norfolk Island", "North Korea", "Northern Marianas",
-	"Norway", "Oman", "Pakistan", "Palau", "Panama", "Papua New Guinea", "Paraguay", "Peru",
-	"Philippines", "Pitcairn Islands", "Poland", "Portugal", "Puerto Rico", "Qatar",
-	"Reunion", "Romania", "Russia", "Rwanda", "Sqo Tome and Principe", "Saint Helena",
-	"Saint Kitts and Nevis", "Saint Lucia", "Saint Pierre and Miquelon",
-	"Saint Vincent and the Grenadines", "Samoa", "San Marino", "Saudi Arabia", "Senegal",
-	"Seychelles", "Sierra Leone", "Singapore", "Slovakia", "Slovenia", "Solomon Islands",
-	"Somalia", "South Africa", "South Georgia and the South Sandwich Islands", "South Korea",
-	"Spain", "Sri Lanka", "Sudan", "Suriname", "Svalbard and Jan Mayen", "Swaziland", "Sweden",
-	"Switzerland", "Syria", "Taiwan", "Tajikistan", "Tanzania", "Thailand", "The Bahamas",
-	"The Gambia", "Togo", "Tokelau", "Tonga", "Trinidad and Tobago", "Tunisia", "Turkey",
-	"Turkmenistan", "Turks and Caicos Islands", "Tuvalu", "Virgin Islands", "Uganda",
-	"Ukraine", "United Arab Emirates", "United Kingdom",
-	"United States", "United States Minor Outlying Islands", "Uruguay", "Uzbekistan",
-	"Vanuatu", "Vatican City", "Venezuela", "Vietnam", "Wallis and Futuna", "Western Sahara",
-	"Yemen", "Yugoslavia", "Zambia", "Zimbabwe"
-    };
 }
